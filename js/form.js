60c133f1c332dde21e6fd1a5ee83bd0b06a06a09
Daniel Platek <danny.platek@gmail.com>
2017-04-07 16:00:48 +0200

    Form app-update: progress bar, steps form number

--- js/form.js
+++ js/form.js
@@ -10,6 +10,10 @@ $(function(){
     var nameFromInput;
     var messageFromInput;
 
+    var $progressBar = $('.progress-bar span');
+    var $stepsFormNC = $('.stepsFormNumber_current');
+    var $stepsFormN  = $('.stepsFormNumber');
+
     $nextButton.on('click', function(e) {
         e.preventDefault();
         var $this = $(this);
@@ -46,6 +50,10 @@ $(function(){
                 $email.text('e-mail: ' + mailFromInput);               //Printe e-mail
                 $msgInput.attr('placeholder', 'Whats your message?');  //Change placeholder
                 $this.data('clicked', 2);                              //Set up data 'clicked' = 2
+
+                $progressBar.css({'width': '66%'});
+                $stepsFormNC.text('3 of');
+
             }else {
                 $errorMsg.text('Please enter the correct e-mail.');
             }
@@ -56,8 +64,12 @@ $(function(){
             if ( $msgInput.val().length > 0 ) {
                 messageFromInput = $msgInput.val();
                 $errorMsg.text('');
+                // $progressBar.css({'width': '100%'}); // If we don't hide the form
                 $msgInput.hide();
                 $nextButton.hide();
+                $progressBar.hide();
+                $stepsFormN.hide();
+
                 $message.text('Message: ' + messageFromInput);
                 $errorMsg.text('WebsiteName, Thank you ' + nameFromInput + ' for your message.');
                 sendEmail();
@@ -75,6 +87,9 @@ $(function(){
                 $name.text('Name: ' + nameFromInput);
                 $msgInput.attr('placeholder', 'Whats your email?');
                 $this.data('clicked', 1);
+
+                $progressBar.css({'width': '33%'});
+                $stepsFormNC.text('2 of');
 
             }else{
                 $errorMsg.text('Please enter the correct name. Min 3 characters.');
